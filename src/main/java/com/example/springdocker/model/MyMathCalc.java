5ed2a52af3d02d26c9f1d504eb595837d4f7f716
Yohannes2010 <72087660+Yohannes2010@users.noreply.github.com>
2021-05-16 15:32:58 +0200

    mathcalc metoder skapas (#1)
    * metoder skapas
    
    * changed to public

---
+++ src/main/java/com/example/springdocker/model/MyMathCalc.java
@@ -0,0 +1,17 @@
+package com.example.springdocker.model;
+
+public class MyMathCalc {
+    public int a;
+    public int b;
+
+    public MyMathCalc () {}
+public int add(int a,int b){
+ return a+b;
+}
+public int multiply (int a,int b){
+        return a*b;
+}
+public int divide(int a,int b){
+        return (a/b);
+}
+}
